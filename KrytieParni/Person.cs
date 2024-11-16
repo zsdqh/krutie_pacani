cb8e4dfa5af8b4bdac24503ceec2fadcdc41e743
angelwittaknife <lesenkoartem315@gmail.com>
2024-11-16 11:16:59 +0300

    Добавил поля и методы класса Person

--- KrytieParni/Person.cs
+++ KrytieParni/Person.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace KrytieParni
 {
-    internal class Person
+    class Person
     {
+        string name;
+        string surname;
+        DateTime birth;
+        public string Name
+        {
+            get { return name; }
+            set {
+                if (value.Length > 2 && value.Length < 20)
+                    name = value;
+                else
+                    throw new Exception("Не допустимая длинна имени");
+            }
+        }
+        public string Surname
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length > 2 && value.Length < 20)
+                    surname = value;
+                else
+                    throw new Exception("Не допустимая длинна фамилии");
+            }
+        }
+        public DateTime Birth
+        {
+            get
+            { return birth; }
+            set
+            {
+                if(value.Year<DateTime.Now.Year)
+                    birth = value;
+                else
+                    throw new Exception("Не допустимая дата рождения");
+            }
+        }
+        public int BirthYear
+        {
+            get
+            { return birth.Year; }
+            set
+            {
+                if (value < DateTime.Now.Year)
+                    birth = new DateTime(value,birth.Month,birth.Day);
+                else
+                    throw new Exception("Не допустимый год рождения");
+            }
+        }
     }
 }
