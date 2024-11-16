c546c69afc9d54f5cb2444b1df8d2bca19b285c6
ArtemVolkov12321 <artemvolkovolegovich@gmail.com>
2024-11-16 14:33:49 +0300

    Средчеко Main 14.33 16.11.24

--- KrytieParni/Main.cs
+++ KrytieParni/Main.cs
@@ -11,7 +11,52 @@ namespace KrytieParni
     {
         public static void Main(string[] args)
         {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+                int n = 10;
+
+                for (int i = n / 2; i <= n; i += 2)
+                {
+                    for (int j = 1; j < n - i; j += 2)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    for (int j = 1; j <= n - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                for (int i = n; i >= 1; i--)
+                {
+                    for (int j = i; j < n; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 1; j <= (i * 2) - 1; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+            
         }
+
     }
 }
+
